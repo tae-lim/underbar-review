3545a58636e60ed9fb7ef22819cd95f5ef178c32
tae-lim <taelim92@gmail.com>
2018-07-31 15:53:07 -0700

    Corrected map spec

--- spec/part1.js
+++ spec/part1.js
@@ -380,7 +380,9 @@
       });
 
       it('should apply a function to every value in an array', function() {
-        var multiplyByTwo = FILL_ME_IN;
+        var multiplyByTwo = function(val) {
+          return val * 2;
+        };
 
         expect(_.map([1, 2, 3], multiplyByTwo)).to.eql([2, 4, 6]);
       });
