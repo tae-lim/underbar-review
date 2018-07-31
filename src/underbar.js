5b0daf6b5306f78837c9da33303cf0c4f3dacb2a
tae-lim <taelim92@gmail.com>
2018-07-31 11:17:10 -0700

    Completed reject function

--- src/underbar.js
+++ src/underbar.js
@@ -80,12 +80,23 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        passed.push(item);
+      }
+    });
+
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function (item) {
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
