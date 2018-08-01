765bcc991fbd48f8a3ce2507cee4dea821820a0a
tae-lim <taelim92@gmail.com>
2018-07-31 17:03:37 -0700

    Fixed git specs for defaults

--- spec/part2.js
+++ spec/part2.js
@@ -265,9 +265,14 @@
       });
 
       it('should copy any property whose key is not already set on the target', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {};
+        var source = { a: 1, b: 2, c: 3};
+
+        _.defaults(destination, source);
+
+        expect(destination.a).to.equal(1);
+        expect(destination.b).to.equal(2);
+        expect(destination.c).to.equal(3);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
